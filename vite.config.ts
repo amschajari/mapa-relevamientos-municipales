6ff26c19feac4e1fb88ee166b36378fd68a7b3e6
Coordinador Relevamiento <coordinador@municipalidad-chajari.gob.ar>
2026-03-11 01:25:22 -0300

    Configurar GitHub Pages con GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mapa-relevamientos-municipales/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
